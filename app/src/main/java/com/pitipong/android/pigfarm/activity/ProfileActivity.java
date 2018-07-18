ed635cc5d5ea2e9e9974edbb4bae135fccccc8b9
BangPanSoft <softwareengineer53@gmail.com>
2018-07-18 21:28:58 +0700

    Init view for profile activity

--- app/src/main/java/com/pitipong/android/pigfarm/activity/ProfileActivity.java
+++ app/src/main/java/com/pitipong/android/pigfarm/activity/ProfileActivity.java
@@ -3,11 +3,19 @@ package com.pitipong.android.pigfarm.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.pitipong.android.pigfarm.R;
 
 public class ProfileActivity extends BaseActivity {
+
+    private EditText
+            edittextName,
+            edittextLastName,
+            edittextOldPassword,
+            edittextNewPassword,
+            edittextConfirmNewPassword;
 
     private ImageView imageViewBack;
 
@@ -20,6 +28,17 @@ public class ProfileActivity extends BaseActivity {
 
     private void initView(){
         imageViewBack = findViewById(R.id.imageViewBack);
+
+        edittextName = findViewById(R.id.edittextName);
+        edittextLastName = findViewById(R.id.edittextLastName);
+        edittextOldPassword = findViewById(R.id.edittextOldPassword);
+        edittextNewPassword = findViewById(R.id.edittextNewPassword);
+        edittextConfirmNewPassword = findViewById(R.id.edittextConfirmNewPassword);
+
+        initClickEvent();
+    }
+
+    private void initClickEvent(){
         imageViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
